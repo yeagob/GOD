646464a23bdf958af6e2ed84c0e38b381f7e1d3e
Santiago Dopazo Hilario <elmundoesazul@gmail.com>
2025-05-28 18:04:07 +0200

    Update MatchData to be JSON serializable with additional match metadata

--- Assets/Scripts/Network/MatchData.cs
+++ Assets/Scripts/Network/MatchData.cs
@@ -1,16 +1,35 @@
+using System;
+
 namespace Network
 {
-    public readonly struct MatchData
+    [Serializable]
+    public struct MatchData
     {
-        public readonly string _id;
-        public readonly string _url;
-        public readonly int _state;
+        public string _id;
+        public string _url;
+        public int _state;
+        public string _createdAt;
+        public int _maxPlayers;
+        public string _gameMode;
 
         public MatchData(string id, string url, int state)
         {
             _id = id;
             _url = url;
             _state = state;
+            _createdAt = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ");
+            _maxPlayers = 4;
+            _gameMode = "classic";
+        }
+
+        public MatchData(string id, string url, int state, int maxPlayers, string gameMode)
+        {
+            _id = id;
+            _url = url;
+            _state = state;
+            _createdAt = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ");
+            _maxPlayers = maxPlayers;
+            _gameMode = gameMode;
         }
     }
 }
