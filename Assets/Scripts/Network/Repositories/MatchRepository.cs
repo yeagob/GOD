4f384f98fe547be8e66489fc69031b8944445c5b
Santiago Dopazo Hilario <elmundoesazul@gmail.com>
2025-05-28 18:07:43 +0200

    Update MatchRepository to use NetworkConstants for Firebase paths

--- Assets/Scripts/Network/Repositories/MatchRepository.cs
+++ Assets/Scripts/Network/Repositories/MatchRepository.cs
@@ -1,13 +1,13 @@
 using System;
 using System.Collections.Generic;
 using Network.Services;
+using Network.Infrastructure;
 
 namespace Network.Repositories
 {
     public class MatchRepository : IMatchRepository
     {
         private readonly IFirebaseService _firebaseService;
-        private const string PATH = "matches";
 
         public MatchRepository(IFirebaseService firebaseService)
         {
@@ -16,7 +16,7 @@ namespace Network.Repositories
 
         public void CreateMatch(MatchData matchData, Action<bool> callback = null)
         {
-            string path = $"{PATH}/{matchData._id}";
+            string path = $"{NetworkConstants.MATCHES_PATH}/{matchData._id}";
             _firebaseService.SetData(path, matchData, callback);
         }
 
@@ -27,25 +27,25 @@ namespace Network.Repositories
 
         public void GetMatch(string matchId, Action<MatchData> callback)
         {
-            string path = $"{PATH}/{matchId}";
+            string path = $"{NetworkConstants.MATCHES_PATH}/{matchId}";
             _firebaseService.GetData<MatchData>(path, callback);
         }
 
         public void ListenForMatchChanges(string matchId, Action<MatchData> callback)
         {
-            string path = $"{PATH}/{matchId}";
+            string path = $"{NetworkConstants.MATCHES_PATH}/{matchId}";
             _firebaseService.ListenForChanges(path, callback);
         }
 
         public void StopListeningForMatch(string matchId)
         {
-            string path = $"{PATH}/{matchId}";
+            string path = $"{NetworkConstants.MATCHES_PATH}/{matchId}";
             _firebaseService.StopListening(path);
         }
 
         public string GenerateMatchId()
         {
-            return _firebaseService.GenerateId(PATH);
+            return _firebaseService.GenerateId(NetworkConstants.MATCHES_PATH);
         }
     }
 }
