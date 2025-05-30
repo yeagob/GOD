e07250063076dd100e4f5d779386db1e2c54a1e2
Santiago Dopazo Hilario <elmundoesazul@gmail.com>
2025-05-30 12:36:23 +0200

    Update NetworkConstants with correct match states: WaitingForPlayers, PlayGame, EndGame

--- Assets/Scripts/Network/Infrastructure/NetworkConstants.cs
+++ Assets/Scripts/Network/Infrastructure/NetworkConstants.cs
@@ -8,9 +8,9 @@ namespace Network.Infrastructure
         public const string PLAYER_MATCHES_PATH = "player_matches";
         public const string GAME_EVENTS_PATH = "game_events";
         
-        public const int MATCH_STATE_WAITING = 0;
-        public const int MATCH_STATE_PLAYING = 1;
-        public const int MATCH_STATE_FINISHED = 2;
+        public const int MATCH_STATE_WAITING_FOR_PLAYERS = 0;
+        public const int MATCH_STATE_PLAY_GAME = 1;
+        public const int MATCH_STATE_END_GAME = 2;
         public const int MATCH_STATE_CANCELLED = 3;
         
         public const int DEFAULT_MAX_PLAYERS = 4;
