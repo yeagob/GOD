61d13f4fb5ade2fe1911a851c2c29c26a5c87c2f
Santiago Dopazo Hilario <elmundoesazul@gmail.com>
2025-05-30 12:28:52 +0200

    Update IMatchModel interface to include match state management

--- Assets/Scripts/Network/Models/IMatchModel.cs
+++ Assets/Scripts/Network/Models/IMatchModel.cs
@@ -5,10 +5,18 @@ namespace Network.Models
 {
     public interface IMatchModel
     {
+        MatchState CurrentMatchState { get; }
+        
         void CreateMatch(string url, int state, Action<MatchData> callback = null);
         void UpdateMatchState(string matchId, int newState, Action<bool> callback = null);
         void GetMatch(string matchId, Action<MatchData> callback);
         void ListenForMatchChanges(string matchId, Action<MatchData> callback);
         void StopListeningForMatch(string matchId);
+        
+        void SetAsHost(string matchId);
+        void SetAsClient(string matchId);
+        void ClearMatchState();
+        bool IsCurrentlyInMatch();
+        bool IsHost();
     }
 }
